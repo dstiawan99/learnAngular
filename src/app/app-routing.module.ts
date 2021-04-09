f218f565dedf15159bcbb1ea0eef1c58eaeab637
dstiawan99 <rico.dstiawan@gmail.com>
2021-04-09 22:46:13 +0700

    add Routing and NAvigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClassComponent } from './class/class.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'class', component: ClassComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [ClassComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LearnComponent } from './learn/learn.component';
 import { FormsModule } from '@angular/forms';
+import { ClassComponent } from './class/class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LearnComponent
+    LearnComponent,
+    ClassComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
